1419bfc15745dbe0c8e003a43a1ddbfc2c910783
EC2 Default User <ec2-user@ip-172-31-55-248.ec2.internal>
2021-02-07 15:13:35 +0000

    Added Flask/html/css functionality

--- app.py
+++ app.py
@@ -3,13 +3,34 @@ import random
 from flask import Flask, render_template
 from spoti import get_info
 
-# These are temp: but belong to Arianna Grande, Lyn (Persona 5 OST), and Hollywood Principle 
-ids = ['66CXWjxzNUsdJxJ2JdwvnR', '5qEtLvXzYdv0G7c7rR6irX', '6ldZGvFDjs6KafLouTBHJ9']
 
-idx = random.randint(0, len(ids) - 1)
 
-# Pass the random index through to get info for a randomly chosen artist. 
-info = get_info(ids[idx])
+app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
-for i in info:
-    print(i)
+@app.route('/')
+
+def hello_world():
+    """ Returns root endpoint HTML """
+    
+    # These are temp: but belong to Arianna Grande, Lyn (Persona 5 OST), and Hollywood Principle 
+    ids = ['66CXWjxzNUsdJxJ2JdwvnR', '5qEtLvXzYdv0G7c7rR6irX', '6ldZGvFDjs6KafLouTBHJ9']
+
+    idx = random.randint(0, len(ids) - 1)
+
+    # Pass the random index through to get info for a randomly chosen artist. 
+    info = get_info(ids[idx])
+    
+    return render_template(
+        "index.html",
+       song_name=info[0],
+       artist_name=info[1],
+       song_img=info[2],
+       song_preview=info[3],
+    )
+# Runs the app
+app.run(
+    host=os.getenv('IP', '0.0.0.0'),
+    port=int(os.getenv('PORT', 8080)),
+    debug=True
+)
